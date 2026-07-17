dce8932ac43751cefd1ec9e5ad2b7f3404b438dc
RiteshByteX <riteshraj1860@gmail.com>
2026-07-17 16:40:43 +0530

    Add showLogin state to context provider

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -3,9 +3,10 @@ import { AppContext } from "./AppContext";
 
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   return (
-    <AppContext.Provider value={{ user, setUser }}>
+    <AppContext.Provider value={{ user, setUser, setShowLogin,showLogin }}>
       {children}
     </AppContext.Provider>
   );
