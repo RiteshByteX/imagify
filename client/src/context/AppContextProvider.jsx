1783f3219de1fc009d6a339badc0564687e981d6
RiteshByteX <riteshraj1860@gmail.com>
2026-07-17 14:21:07 +0530

    Initial commit

---
+++ client/src/context/AppContextProvider.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { AppContext } from "./AppContext";
+
+const AppContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  return (
+    <AppContext.Provider value={{ user, setUser }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
